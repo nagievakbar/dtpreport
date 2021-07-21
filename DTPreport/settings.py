cb0b89b9012d09ef616a1fe691300d331628ac59
Bob <bob@example.com>
2021-07-21 21:00:24 +0500

    ASD

--- DTPreport/settings.py
+++ DTPreport/settings.py
@@ -42,7 +42,7 @@ ALLOWED_HOSTS = ['makereport.elite-house.uz',
                  '184.196.214.55',
                  '89.236.227.206'
                  ]
-URL_FILES = 'http://e-otsenka.uz'
+URL_FILES = 'https://e-otsenka.uz'
 BASE_URL = URL_FILES + "/media/"
 # Application definition
 
